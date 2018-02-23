1377caeeff44b3abb26f39997aba9dbb0084de15
Dalkyan <gnyrek@gmail.com>
2018-02-23 21:04:07 +0100

    piateczek

---
+++ src/czwarte/Arrays.java
@@ -0,0 +1,80 @@
+package czwarte;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int size = 5;
+        int [] descendingNumbers new int[size];
+        for (int i = 0; i < ; i++) {
+            descendingNumbers[i] = descendingNumbers.length - i;
+                    }
+        System.out.println(Arrays.toString(descendingNumbers));
+
+       int[] arrayWithValue = {1, 4, 5, 7, -4, 2};
+       int counter = 0;
+       while (counter < arrayWithValue.length && arrayWithValue[counter] >= 0 )
+       { counter++;
+       }
+        System.out.println("In index " + counter + " There is a minus value: " + arrayWithValue[counter]);
+int[] arrayToBeDoubled = {1,2,4,57,89};
+        for (int i = 0; i < arrayToBeDoubled.length; i++) {
+            System.out.print(arrayToBeDoubled[i] + " becomes ");
+            arrayToBeDoubled[i] *= 2;
+            System.out.print(arrayToBeDoubled[i]);
+            System.out.println();
+        }
+        int n = 4;
+                int[] newArray = new int[n];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = i;
+        }
+        System.out.println(Arrays.toString(newArray));
+        int sum =0;
+        int array[] = {1,2,3,4,5,6};
+
+        for (int i=0; i<array.length; i++)
+        {sum += array[i];
+        }
+        System.out.println(sum);
+        System.out.println(sum/array.length);
+        int min = array[0];
+        int max = array[0];
+
+        for (int i =1; i < array.length; i++)
+        {if (array[i]< min){  //
+            min = array[i];
+        }if (array[i] > max){
+    max = array[i] ;
+                   }
+
+        }
+        System.out.println();
+        System.out.println("The smallest number is " + min + " and " + max + " is the biggest number in this array.");
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        int sizeOfUserArray = scanner.nextInt();
+        int[] userArray = new int[sizeOfUserArray];
+        int sumOfUserArray = 0;
+        for (int i = 0; i < userArray.length; i++) {
+            System.out.println("Insert next value:");
+            userArray[i] = scanner.nextInt();
+            sumOfUserArray += userArray[i];
+
+            int searchingNumber = 6;
+            int [] sortedArray = {1,3,5,6,7,8,9};
+            int counterForSearching = 0;
+            while (sortedArray[counterForSearching] != searchingNumber)
+            {
+                counterForSearching++;
+            }
+            System.out.println("We have " + counterForSearching + "numbers smaller than " + searchingNumber);
+
+        }
+    }
+}
+// napisać pętle, która wypisze znaki zakodowane, druga pętla odkoduje szyfr cezara
+// (nie)posortowana tablica liczb. tworzymy 2 tablice z elementami mniejszymi i większymi niż N.
---
+++ src/czwarte/Scope.java
@@ -0,0 +1,101 @@
+package czwarte;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
+public class Scope {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+
+       int[] ints = new int[5];
+       String[] names = {"Jan", "Anna", "Marek"};
+        for (int i = 0; i < names.length; i++){
+            System.out.println(names[i]);   // <= names[i].sout + enter daje ten kod
+        }
+
+        // scopeOfTypes();
+        decimalToBinary();
+        BinaryToDecimal();
+        String message = "Hello World";
+
+        char[] messageAsCharArray = message.toCharArray();
+        int length = messageAsCharArray.length;
+        System.out.print(messageAsCharArray[0]);
+        System.out.print(messageAsCharArray[1]);
+        System.out.print(messageAsCharArray[5]);
+
+        int [] array = {1,2,3,4,5,7}; //jeżeli raz zdefiniuje długość, rozmiar jest stały
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+
+        int a=97;
+
+        for (int i = 0; i <= 122-97; i+=2){
+            System.out.print((char) (a+ i));
+        }
+
+        for (int i = 97; i<= 122; i++) {
+        System.out.print((char)i);
+    }
+    for (char i = 'z'; i >= 97; i--){
+        System.out.print(i);
+    }
+
+    }
+
+    private static void BinaryToDecimal() {
+        long binaryCode = 101010101011110101L;
+        int doubler = 1;
+        int decimal=0;
+        while (binaryCode != 0)
+        { long digit = binaryCode % 10;
+        binaryCode /=10;
+            decimal += digit * doubler;
+            doubler *= 2;
+        }
+        System.out.println("Decimal= " + decimal);
+    }
+
+    private static void decimalToBinary() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter value to turn into binary:");
+        int number = scanner.nextInt();
+        int binary = 0;
+        int multiplier = 1;
+        while (number != 0) {
+            int digit = number % 2;
+            number /= 2;
+            binary += digit * multiplier;
+            multiplier *= 10;
+        }
+        System.out.println(binary);
+    }
+//}
+//    private static void scopeOfTypes() {
+//        int number = 1500000000;
+//        System.out.println(number + number);
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+//        long bigNumber = 3000000000000L;
+//        System.out.println(bigNumber);
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(Long.MIN_VALUE);
+//
+//        System.out.println(Double.MAX_EXPONENT);
+//        System.out.println(Double.MIN_VALUE);
+//
+//        System.out.println(Float.MAX_VALUE);
+//        System.out.println(Float.MIN_VALUE;
+//        System.out.println(Short.MAX_VALUE);
+//        System.out.println(Short.MIN_VALUE);
+//        System.out.println(Byte.MAX_VALUE);
+//        System.out.println(Byte.MIN_VALUE);
+//
+//        String message = "Hello!";
+//        char character = '$';
+//        System.out.println(character + 3);
+
+}
